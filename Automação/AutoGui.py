a7d145ebc48461e73b82f2d2062517e771327339
Enzo Rodrigues <en.zoo@hotmail.com>
2024-12-06 11:52:52 -0300

    Versão final Repo

---
+++ Automação/AutoGui.py
@@ -0,0 +1,16 @@
+import pyautogui as pt
+import time
+
+
+pt.PAUSE = 0.3
+
+# abrir o navegador (chrome)
+pt.press("win")
+pt.write("notepad")
+pt.press("enter")
+
+# pt.write("https://www.youtube.com/")
+# pt.press("enter")
+
+# time.sleep(3)
+pt.click( 200 ,  220 )
